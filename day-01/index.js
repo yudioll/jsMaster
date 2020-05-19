c843bf6712a8ad38baf931fd9e4ee28b32f0fe81
yudioll <1227795790@qq.com>
2020-05-19 23:51:34 +0800

    yudioll:jsMaster--性能分析简单示例

---
+++ day-01/index.js
@@ -0,0 +1,8 @@
+/**
+ * @description 性能分析
+ */
+console.time("My operation"); //开始计时器
+for(var n = 0; n < 10; n++){
+/*perform the operation to be measured*/
+} //执行多次操作
+console.timeEnd("My operation");
